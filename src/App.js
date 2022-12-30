74a157a71a32e46d4cccf128868732c02997dded
Shraddha Goyal <shraddhagoyal800@gmail.com>
2022-12-30 23:20:12 +0530

    CSs file made and Buttons and App component structure is made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
+import Button from "./Buttons";
+import "./index.css";
 
 function App() {
   return (
-   <></>
+    <>
+      <div className="main_div">
+        <div className="inner_div">
+          <h1 className="heading_h1">My customize tooltip</h1>
+          <div className="customize_buttons">
+            <Button value = {"Top"}/>
+            <Button value = {"Right"}/>
+            <Button value = {"Left"}/>
+            <Button value = {"Bottom"}/>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
