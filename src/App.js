5af7eb873d502c8836c5d1c6cb370c3443ea324f
Shraddha Goyal <shraddhagoyal800@gmail.com>
2022-12-31 23:20:08 +0530

    Tooltip componnent is completed and styling of tooltip is also completed.

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import "./index.css";
 function App() {
   return (
     <>
-      <div className="main_div">
-        <div className="inner_div">
-          <h1 className="heading_h1">My customize tooltip</h1>
-          <div className="customize_buttons">
-            <Button value = {"Top"} bgColor = {"pink"}/>
-            <Button value = {"Right"} bgColor = {"blue"}/>
-            <Button value = {"Left"} bgColor = {"crimson"}/>
-            <Button value = {"Bottom"} bgColor = {"orange"}/>
-          </div>
+      <div className="inner_div">
+        <h1 className="heading_h1">My customize tooltip</h1>
+        <div className="customize_buttons">
+          {/* fetching alll the buttons from the button component */}
+          <Button value={"top"} bgColor={"pink"} id={1} />
+
+          <Button value={"right"} bgColor={"blue"} id={2} />
+
+          <Button value={"left"} bgColor={"crimson"} id={3} />
+
+          <Button value={"bottom"} bgColor={"orange"} id={4} />
         </div>
       </div>
     </>
--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { useState } from "react";
+import "./index.css";
 
 const Button = (props) => {
+  const [ishovered, setHovered] = useState(false);
+
+  // callback function for onMouse enter and leave
+  const onHover = () => {
+    setHovered(true);
+    console.log(props.id);
+  };
+  const onLeave = () => {
+    setHovered(false);
+    console.log("leave", props.id);
+  };
   return (
     <>
-      <button style = {{backgroundColor : props.bgColor}}>
+      {/* button compoent */}
+      <button
+        style={{ backgroundColor: props.bgColor }}
+        onMouseEnter={onHover}
+        onMouseLeave={onLeave}
+        className="tooltip"
+      >
         Hover for {props.value}
+        <span
+          style={{ visibility: ishovered ? "visible" : "hidden" }}
+          // className="tooltiptext top"
+          className={`tooltiptext ${props.value}`}
+        >
+          Thanks for Hovering ! I am a {props.value} position tooltip
+        </span>
       </button>
     </>
   );
